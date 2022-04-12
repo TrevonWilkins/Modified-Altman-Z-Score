0d0e4cfec50f34418ccf802c4beebb2c9471fa1c
Trevon <90734292+TrevonWilkins@users.noreply.github.com>
2022-04-12 16:25:19 -0400

    UPDATE

--- altz_score.py
+++ altz_score.py
@@ -9,8 +9,7 @@ from yahoofinancials import YahooFinancials
 '''
 This is a modified Altman Z-Score function that substitutes total revenue in place of net sales. Total revenue is used in place of net sales as most organizations do not publicize their net sales. This function works particularly well for defense contracting organizations where discounts, returns, and allowances are not a large part of the business due to the contractual nature of the work, thus the alteration is ideally insignificant enough to provide a well-rounded gauge of probability of bankruptcy when compared to organizations of the same business interest.
 
-Traditional Altman Z-Score
-= 1.2A + 1.4B + 3.3C + 0.6D + 1.0E
+"Altman Z-Score = 1.2A + 1.4B + 3.3C + 0.6D + 1.0E
 
 Where:
 
@@ -18,12 +17,13 @@ A = working capital / total assets
 B = retained earnings / total assets
 C = earnings before interest and tax / total assets
 D = market value of equity / total liabilities
-E = sales / total assets
+E = sales / total assets "
+
+Cite:
+
+Kenton, W. (2003, November 18). What is the Altman Z-score? Investopedia. Retrieved April 12, 2022, from https://www.investopedia.com/terms/a/altman.asp#:~:text=The%20formula%20for%20Altman%20Z,*(sales%20%2F%20total%20assets)
 
 *With modification E = (total revenue / total assets)
-
-Cite: 
-https://www.investopedia.com/terms/a/altman.asp#:~:text=The%20formula%20for%20Altman%20Z,*(sales%20%2F%20total%20assets).
 
 How To Use:
 
