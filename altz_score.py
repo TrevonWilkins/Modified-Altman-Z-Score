50bc43f635a46ef00a915430d7fcd1c3434ec2ee
Trevon <90734292+TrevonWilkins@users.noreply.github.com>
2022-05-17 01:47:52 -0400

    Modified Score

--- altz_score.py
+++ altz_score.py
@@ -5,11 +5,11 @@
 import yfinance as yf
 from yahoofinancials import YahooFinancials
 
-
 '''
-This is a modified Altman Z-Score function that substitutes total revenue in place of net sales. Total revenue is used in place of net sales as most organizations do not publicize their net sales. This function works particularly well for defense contracting organizations where discounts, returns, and allowances are not a large part of the business due to the contractual nature of the work, thus the alteration is ideally insignificant enough to provide a well-rounded gauge of probability of bankruptcy when compared to organizations of the same business interest.
+This is a modified Altman Z-Score function that substitutes total revenue in place of net sales. Total revenue is used in place of net sales as most organizations do not publicize their net sales. This function works particularly well for defense contracting organizations where discounts, returns, and allowances are not a large part of the business due to the contractual nature of the work, thus may be insignificant enough to provide a well-rounded gauge of bankruptcy risk when compared to organizations of the same business interest.
 
-"Altman Z-Score = 1.2A + 1.4B + 3.3C + 0.6D + 1.0E
+Traditional Altman Z-Score
+= 1.2A + 1.4B + 3.3C + 0.6D + 1.0E
 
 Where:
 
@@ -17,13 +17,12 @@ A = working capital / total assets
 B = retained earnings / total assets
 C = earnings before interest and tax / total assets
 D = market value of equity / total liabilities
-E = sales / total assets "
-
-Cite:
-
-Kenton, W. (2003, November 18). What is the Altman Z-score? Investopedia. Retrieved April 12, 2022, from https://www.investopedia.com/terms/a/altman.asp#:~:text=The%20formula%20for%20Altman%20Z,*(sales%20%2F%20total%20assets)
+E = sales / total assets
 
 *With modification E = (total revenue / total assets)
+
+Cite: 
+https://www.investopedia.com/terms/a/altman.asp#:~:text=The%20formula%20for%20Altman%20Z,*(sales%20%2F%20total%20assets).
 
 How To Use:
 
@@ -34,25 +33,27 @@ Output: MSFT modified Altman Z-Score = X.XX
 '''
 
 def altz_score(x):
-    ticker = yf.Ticker(x)
-    ticker_financials = ticker.financials
-    ticker_balance = ticker.balance_sheet
     
-    t_rev = float(ticker_financials.loc[['Total Revenue']].iloc[0][:][0])
-    ebit = float(ticker_financials.loc[['Ebit']].iloc[0][:][0])
-    tot_assets= float(ticker_balance.loc[['Total Assets']].iloc[0][:][0])
-    tot_liab = float(ticker_balance.loc[['Total Liab']].iloc[0][:][0])
-    ret_earnings = float(ticker_balance.loc[['Retained Earnings']].iloc[0][:][0])
-    working_cap = float((ticker_balance.loc[['Total Current Assets']].iloc[0][:][0]) - (ticker_balance.loc[['Total Current Liabilities']].iloc[0][:][0]))
-    market_cap = ticker.info['marketCap']
+    try:
+        ticker = yf.Ticker(x)
+        ticker_financials = ticker.financials
+        ticker_balance = ticker.balance_sheet
+
+        t_rev = float(ticker_financials.loc[['Total Revenue']].iloc[0][:][0])
+        ebit = float(ticker_financials.loc[['Ebit']].iloc[0][:][0])
+        tot_assets= float(ticker_balance.loc[['Total Assets']].iloc[0][:][0])
+        tot_liab = float(ticker_balance.loc[['Total Liab']].iloc[0][:][0])
+        ret_earnings = float(ticker_balance.loc[['Retained Earnings']].iloc[0][:][0])
+        working_cap = float((ticker_balance.loc[['Total Current Assets']].iloc[0][:][0]) - (ticker_balance.loc[['Total Current Liabilities']].iloc[0][:][0]))
+        market_cap = ticker.info['marketCap']
+
+        A = working_cap / tot_assets
+        B = ret_earnings / tot_assets
+        C = ebit / tot_assets
+        D = market_cap / tot_liab
+        E = t_rev / tot_assets
+        altz_score = (1.2*A) + (1.4*B) + (3.3*C) + (0.6*D) + (1.0*E)
+        return "{} modified Altman Z-Score = {}".format(x,"{:.2f}".format(altz_score))
     
-    A = working_cap / tot_assets
-    B = ret_earnings / tot_assets
-    C = ebit / tot_assets
-    D = market_cap / tot_liab
-    E = t_rev / tot_assets
-    altz_score = (1.2*A) + (1.4*B) + (3.3*C) + (0.6*D) + (1.0*E)
-    print("{} modified Altman Z-Score = {}".format(x,"{:.2f}".format(altz_score)))
-
-
-
+    except:
+        return "None"
